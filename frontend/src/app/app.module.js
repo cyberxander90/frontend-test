c0d6a6a4dea69a601f50a99ad6609d416678eef6
cyberxander90 <cyberxander90@gmail.com>
2017-04-10 22:53:41 -0400

    remove angular-material and include angular-ui-bootstrap

--- frontend/src/app/app.module.js
+++ frontend/src/app/app.module.js
@@ -1,7 +1,6 @@
 import angular from 'angular';
 import 'angular-ui-router';
 import 'angular-ui-bootstrap';
-import 'angular-media-queries';
 
 import { CommonModule } from './common/common.module';
 import { EventsModule } from './events/events.module';
