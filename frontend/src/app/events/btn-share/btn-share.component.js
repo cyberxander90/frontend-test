6557ee387958641e3eb1ca23eaa253c487a7898d
cyberxander90 <cyberxander90@gmail.com>
2017-04-10 20:10:54 -0400

    twitter support

--- frontend/src/app/events/btn-share/btn-share.component.js
+++ frontend/src/app/events/btn-share/btn-share.component.js
@@ -2,9 +2,21 @@ import './btn-share.scss';
 
 export const BtnShareComponent = {
     template: require('./btn-share.html'),
+    bindings: {
+        event: '<'
+    },
     controller: class BtnShareComponent{
 
-        postToTwitter(){
+        constructor($state){
+            this.$state = $state;
+        }
+
+        $onInit(){
+            var eventDetailsUri = this.$state.href('events.details-event', {id: this.event.id}, {absolute: true})
+
+            this.twitterLink = 'https://twitter.com/intent/tweet?' +
+                'text=' + 'Iré al ' + this.event.title +
+                ' @ ' + this.event.dates[0] + ' ' + encodeURI(eventDetailsUri);
 
         }
     }
